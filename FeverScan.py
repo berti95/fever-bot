6fd84d48880d27cb27595822583bbce459e7b1fe
berti95 <alberto.gonzalez.gomez@hotmail.com>
2016-11-30 01:06:38 +0100

    Update FeverScan.py

--- FeverScan.py
+++ FeverScan.py
@@ -12,7 +12,6 @@ import telebot #necesario para telegram
 from bs4 import BeautifulSoup
 from stem import Signal
 from stem.control import Controller
-#Mensaje antiguo  --> 224218425
 def requestp(url,proxyuse=True):
     user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
     headers = {'User-Agent' : user_agent}
